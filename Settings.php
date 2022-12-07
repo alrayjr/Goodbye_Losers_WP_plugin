88370211e4bb74219713b7cfe9911881b8080d00
Morzel Chambers <sargentnoodles55@gmail.com>
2022-12-06 23:43:10 -0500

    Added MOTIVATION
    Extra motivation to fight the voices

--- Settings.php
+++ Settings.php
@@ -1,21 +1,25 @@
 <?php
 
-
 function lyric_display_section_dev_cb()
 {
+};
+
+function loser_print_motivation()
+{
+	$setting_moto = get_option('loser_display_moto');
+	echo '<p id=motivate>';
+	echo$setting_moto;
+	echo '</p>';
 };
 
 
 function loser_display_setting()
 {
-	$setting_author = get_option('loser_display_author');
-
 	?>
 
-	<input type ="text" name = "loser_display_author">
+	<input type ="text" name = "loser_display_moto">
 
 	<?php
-
 }
 
 
@@ -35,9 +39,12 @@ function loser_display_settings_init()
 	//New Field
 	add_settings_field(
 		'loser_display_settings_motivation',
-		'Motivation', 'loser_display_setting',
+		'Fight Back Against the Voices', 'loser_display_setting',
 		'general',
 		'loser_display_section_dev'
 	);
+	
 }
+
 add_action('admin_init','loser_display_settings_init');
+add_action('admin_notices','loser_print_motivation');
